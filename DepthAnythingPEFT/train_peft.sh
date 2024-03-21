e341ba301b246e665a830175ca66d2ecc9b5f748
Abhimanyu Bhowmik <bhowmikabhimanyu@gmail.com>
2024-03-21 22:37:55 +0100

    Update training scripts

--- DepthAnythingPEFT/train_peft.sh
+++ DepthAnythingPEFT/train_peft.sh
@@ -3,8 +3,8 @@
 #SBATCH --job-name=depthAnthingPEFT
 #SBATCH --output=DepthAnything/scripts/depthAnthingPEFT.out
 #SBATCH --error=DepthAnything/scripts/depthAnthingPEFT.err
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+#SBATCH --partition=mundus
+#SBATCH --gres=gpu:a100-20:1
 #SBATCH --time=06:30:00
 
 python DepthAnything/scripts/run_training.py
