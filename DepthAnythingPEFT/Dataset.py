479c11e9790da0e34acfbf3718ecfa66e0992fbb
Krittapat Onthuam <ktponthuam@gmail.com>
2024-03-27 14:24:48 +0100

    add class VAROSDataset

--- DepthAnythingPEFT/Dataset.py
+++ DepthAnythingPEFT/Dataset.py
@@ -71,3 +71,31 @@ class USODDataset(Dataset):
                 depth = self.transform(depth)
 
             return image, depth
+        
+class VAROSDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.data = self._load_data()
+
+    def _load_data(self):
+        data = []
+        for root, _, files in os.walk(self.root_dir):
+            for file in files:
+                    if 'A' in root:
+                        depth_path = os.path.join(root, file)
+                        image_path = os.path.join(root.replace('A', 'D'), file.replace('A', 'D'))
+                        data.append((image_path, depth_path))
+        return data
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image_path, depth_path = self.data[idx]
+        image = Image.open(image_path)
+        depth = Image.open(depth_path)
+        if self.transform:
+            image = self.transform(image)
+            depth = self.transform(depth)
+        return image, depth
